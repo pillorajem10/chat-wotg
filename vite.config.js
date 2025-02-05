98dffd3495df77262c25be6be2c3c74ce0bdd29c
unknown <pillorajem10@gmail.com>
2025-02-05 13:16:31 +0800

    mod

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+    base: "/vite/", // Ensure all assets are prefixed with /vite/
+    server: {
+        host: "0.0.0.0",
+        strictPort: true,
+        port: 5173,
+        hmr: {
+            host: "chat.wotgonline.com",
+            protocol: "wss"
+        }
+    },
+    build: {
+        outDir: "public/vite",
+        emptyOutDir: true
+    }
 });
+

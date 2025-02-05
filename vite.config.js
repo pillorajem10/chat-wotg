6d4c55403f7ad7567ca97a6246bfe7c36dd495b5
unknown <pillorajem10@gmail.com>
2025-02-05 13:49:35 +0800

    mod

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
     base: "/vite/", // Ensure all assets are prefixed with /vite/
     server: {
         host: "0.0.0.0",
-        port: false,
         hmr: {
             host: "chat.wotgonline.com",
             protocol: "wss"
